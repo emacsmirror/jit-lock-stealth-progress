46d7a8a02a01c81125e35c81fdec5295bb4cbcd2
Campbell Barton <ideasman42@gmail.com>
2023-08-08 23:42:09 +1000

    Improve 'with-advice' using gensym for let bound variable

--- jit-lock-stealth-progress.el
+++ jit-lock-stealth-progress.el
@@ -56,12 +56,13 @@ With a customized mode-line it may be preferable to include
 (defmacro jit-lock-stealth-progress--with-advice (fn-orig where fn-advice &rest body)
   "Execute BODY with WHERE advice on FN-ORIG temporarily enabled."
   (declare (indent 3))
-  `(let ((fn-advice-var ,fn-advice))
-     (unwind-protect
-         (progn
-           (advice-add ,fn-orig ,where fn-advice-var)
-           ,@body)
-       (advice-remove ,fn-orig fn-advice-var))))
+  (let ((function-var (gensym)))
+    `(let ((,function-var ,fn-advice))
+       (unwind-protect
+           (progn
+             (advice-add ,fn-orig ,where ,function-var)
+             ,@body)
+         (advice-remove ,fn-orig ,function-var)))))
 
 
 ;; ---------------------------------------------------------------------------
